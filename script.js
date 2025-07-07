2b0a8c024dba617ee5b943519ab0eae749d708a0
asmita-05 <ashasmita05@gmail.com>
2025-07-08 00:37:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('reveal-email').addEventListener('click', () => {
+  const email = document.getElementById('email');
+  email.style.display = email.style.display === 'none' ? 'block' : 'none';
+});
